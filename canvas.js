74ace07f5b4b78bd1954bdb81afc9bd89c4cb9c0
yichien <kevinyichienlee@gmail.com>
2022-07-22 22:55:55 +0800

    draw arc, rect, line in canvas

---
+++ canvas.js
@@ -0,0 +1,40 @@
+
+const canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const c = canvas.getContext('2d');
+c.fillStyle = 'rgba(255,0,0,0.1)'
+c.fillRect(100, 100, 100, 100);
+c.fillStyle = 'rgba(0,255,0,0.1)'
+c.fillRect(400, 100, 100, 100);
+c.fillStyle = 'rgba(0,0,255,0.1)'
+c.fillRect(300, 300, 100, 100);
+
+
+console.log(canvas);
+
+// Line
+// c.moveTo(x,y);
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "blue"
+c.stroke();
+
+
+// Arc / Circle
+c.beginPath();
+c.arc(300,300,30,0,Math.PI *2, false);
+c.strokeStyle = "green"
+c.stroke();
+
+for (let i = 1 ; i <= 100; i++) {
+  let x = Math.random() * window.innerWidth;
+  let y = Math.random() * window.innerHeight;
+  c.beginPath();
+  c.arc(x, y, 30, 0, Math.PI *2, false);
+  c.strokeStyle = "green"
+  c.stroke();
+}
