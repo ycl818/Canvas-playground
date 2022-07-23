569c5d1bda3a03cbe674a345b4986a84c380466f
yichien <kevinyichienlee@gmail.com>
2022-07-24 01:16:45 +0800

    object program random circles

--- canvas.js
+++ canvas.js
@@ -39,32 +39,65 @@ const c = canvas.getContext('2d');
 //   c.stroke();
 // }
 
-c.beginPath();
-c.arc(200, 200, 30, 0, Math.PI *2, false);
-c.strokeStyle = "green"
-c.stroke();
 
-let x = 200;
-let dx = 4;
-let y = 200;
-let dy = 4;
-let radius = 30;
+// let x = Math.random() * innerWidth;
+// let y = Math.random() * innerHeight;
+// let dx = (Math.random() - 0.5) * 8;
+// let dy = (Math.random() - 0.5) * 8;
+// let radius = 30;
+
+
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = "green"
+    c.stroke();
+  }
+
+  this.update = function() {
+    if (this.x  + this.radius > innerWidth || this.x  - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  }
+}
+
+
+
+let circleArray = [];
+
+for (let i = 0; i < 100; i++) {
+  let radius = 30;
+  let x = Math.random() * (innerWidth - radius * 2) + radius;
+  let y = Math.random() * (innerHeight - radius * 2) + radius;
+  let dx = (Math.random() - 0.5) * 8;
+  let dy = (Math.random() - 0.5) * 8;
+  
+  circleArray.push(new Circle(x, y, dx, dy, radius))
+  
+}
+
+
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
 
-  c.beginPath();
-  c.arc(x, y, 30, 0, Math.PI *2, false);
-  c.strokeStyle = "green"
-  c.stroke();
-  if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx;
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
   }
-  if (y + radius > innerHeight || y - radius < 0) {
-    dy = -dy;
-  }
-  x += dx;
-  y += dy;
 }
 
-//animate();
+animate();
